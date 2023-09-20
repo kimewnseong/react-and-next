83c7f7306da8337ce6fcfce19e97e795c92ec368
kimewnseong <rladbstjd96@naver.com>
2023-09-20 13:26:14 +0900

    feat: add search bar component

---
+++ react-naras/src/components/SearchBar.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import style from './SearchBar.module.css';
+
+function SearchBar() {
+  const navigate = useNavigate();
+
+  const [search, setSearch] = useState('');
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onClickSearch();
+    }
+  };
+  const onClickSearch = () => {
+    if (search) {
+      navigate(`/search?search=${search}`);
+    }
+  };
+
+  return (
+    <div className={style.container}>
+      <input
+        placeholder="검색어를 입력하세요."
+        value={search}
+        onChange={onChangeSearch}
+        onKeyDown={onKeyDown}
+      />
+      <button onClick={onClickSearch}>검색</button>
+    </div>
+  );
+}
+
+export default SearchBar;
