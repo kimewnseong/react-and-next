7679dca78b246d01f87ed27e9f82c80018dc58a8
kimewnseong <rladbstjd96@naver.com>
2023-09-21 18:52:26 +0900

    fix: Search page CSR방식으로 수정

--- next-naras/pages/search/index.js
+++ next-naras/pages/search/index.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import { getSearchCountries } from '@/api';
 import SubLayout from '@/components/SubLayout';
 
-function Search({ countries }) {
+function Search() {
+  const router = useRouter();
+  const { search } = router.query;
+  const [countries, setCountries] = useState([]);
+
+  const setData = async () => {
+    const data = await getSearchCountries(search);
+    setCountries(data);
+  };
+
+  useEffect(() => {
+    if (search) {
+      setData();
+    }
+  }, [search]);
+
   return (
     <div>
       {countries.map((country) => (
@@ -14,19 +31,3 @@ function Search({ countries }) {
 export default Search;
 
 Search.Layout = SubLayout;
-
-export const getServerSideProps = async (context) => {
-  // 쿼리
-  const { search } = context.query;
-  let countries = [];
-
-  if (search) {
-    countries = await getSearchCountries(search);
-  }
-
-  return {
-    props: {
-      countries,
-    },
-  };
-};
