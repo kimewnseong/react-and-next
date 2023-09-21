b4241fea69294ca0215c5dc95f2ce85adb019a08
kimewnseong <rladbstjd96@naver.com>
2023-09-21 18:43:01 +0900

    feat: SSG방식으로 정적 페이지 생성
    getStaticPaths를 이용해서 동적 라우팅을 갖는 정적 페이지 처리

--- next-naras/pages/country/[code].js
+++ next-naras/pages/country/[code].js
@@ -5,14 +5,42 @@ import SubLayout from '@/components/SubLayout';
 function Country({ country }) {
   const router = useRouter();
 
-  return <div>Country : {country.officialName}</div>;
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  if (!country) {
+    return <div>존재하지 않는 국가입니다.</div>;
+  }
+
+  return (
+    <div>
+      {country.commonName} : {country.officialName}
+    </div>
+  );
 }
 
 export default Country;
 
 Country.Layout = SubLayout;
 
-export const getServerSideProps = async (context) => {
+// 동적 경로를 갖는 페이지에 추가
+export const getStaticPaths = async () => {
+  return {
+    paths: [{ params: { code: 'KOR' } }, { params: { code: 'ABW' } }],
+    fallback: true,
+    // fallback : 위의 경로에 해당하지 않는 옵션이 올 때 처리 할 내용
+    // fallback: false => 404 리턴
+
+    // fallback: "blocking" => 존재하지 않는 경로 요청 시, SSR처럼 해당 페이지 실시간 생성 후 서버에 저장.(생성동안은 로딩..)
+    // 다음 번에 다시 요청 시, SSG처럼 동작
+
+    // fallback: true => 데이터 없는 페이지 먼저 보여준 후에
+    // prop계산을 마치고 데이터가 있는 페이지 보여줌
+  };
+};
+
+export const getStaticProps = async (context) => {
   const { code } = context.params;
   let country = null;
 
